2c6301b3ec8541b68dba25a1e7bb9e34766d8390
João Inácio <egnawake@gmail.com>
2023-04-30 20:18:26 +0100

    Fix info panel positioning

--- Assets/Scripts/FishInfoPanel.cs
+++ Assets/Scripts/FishInfoPanel.cs
@@ -3,12 +3,26 @@ using TMPro;
 
 public class FishInfoPanel : MonoBehaviour
 {
+    [SerializeField] private SMFishes parentFish;
     [SerializeField] private TMP_Text energyText;
+    [SerializeField] private Vector3 offset;
 
-    public float Energy {
+    public float Energy
+    {
         set
         {
             energyText.text = $"Energy: {value.ToString("F0")}";
         }
+    }
+
+    private void Start()
+    {
+        Energy = parentFish.Energy;
+        parentFish.EnergyChanged.AddListener(() => Energy = parentFish.Energy);
+    }
+
+    private void Update()
+    {
+        transform.position = parentFish.transform.position + offset;
     }
 }
